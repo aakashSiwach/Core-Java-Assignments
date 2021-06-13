28fa965bb89fe5ea19a725f46829228a373f5b97
aakash855 <55450641+aakash855@users.noreply.github.com>
2021-06-13 15:27:19 +0530

    Add files via upload

---
+++ comment.java
@@ -0,0 +1,21 @@
+import java.util.*;
+/**
+ * This is a single line Comment
+ * @author Aakash
+
+ */
+
+class comment
+{
+    public static void main (String [] args){
+//This is a single line Comment
+System.out.println("Single Line Comment");
+/*
+This is a multi line Comment
+*/
+
+System.out.println("Multi Line Comment");
+
+    }
+
+}
---
+++ function.java
@@ -0,0 +1,14 @@
+class function 
+{
+
+	public static void main(String[] args)
+	{
+	      name();
+	}
+
+	static void name()
+	{
+		System.out.println("Aakash");
+	}
+
+}
